ab58806188a859b0cf0d851eb10483a5d2132995
Andrey Bakulin <hansss@list.ru>
2023-12-11 23:50:31 +0400

    step 2.5

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,13 +3,9 @@ import Header from './Header';
 
 const Layout = ({ children }) => {
   return (
-    <div className="h-100">
-      <div className="h-100">
-        <div className="d-flex flex-column h-100">
-          <Header />
-          <div className="container-fluid h-100">{children}</div> 
-        </div>
-      </div>
+    <div className="d-flex flex-column h-100">
+      <Header />
+      <div className="container-fluid h-100">{children}</div>
     </div>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import init from './init.jsx';
 
 const app = async () => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
+  const root = ReactDOM.createRoot(document.getElementById('chat'));
   root.render(await init());
 };
 
